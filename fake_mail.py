465417e40689309df6de6e0a6e4a210233a90209
RawanShareef <73065095+rawanshareef@users.noreply.github.com>
2022-11-08 11:24:27 +0200

    Add files via upload

---
+++ fake_mail.py
@@ -0,0 +1,37 @@
+import sys
+
+data = sys.argv
+username = data[1]
+mail_service_name = data[2]  # like "@yahoo.com" or "@google.com" etc.
+title = data[3]
+job_title = data[4]
+personal_status = data[5]
+kids = data[6]
+
+kid = []
+try:
+    if kids == 'yes':
+        for kids in range(7, len(sys.argv)):
+            kid.append(data[kids])
+
+        int(data[len(sys.argv) - 1])  # if it works so there is no email from Joseph
+        print("No email from Joseph.\n")
+
+    else:
+        # in case the last argument is an email from Joseph
+        if data[len(sys.argv) - 1] == "no":
+            print("No email from Joseph.\n")
+        else:
+            raise TypeError("")
+
+
+except:
+    print("We got an email from Joseph:\n")
+    email = data[len(sys.argv) - 1]
+    print(data)
+
+print(f'username = {data[1]}\nmail_service_name = {data[2]}\ntitle = {data[3]}\njob_title = {data[4]}\n'
+      f'personal_status = {data[5]}\nkids = {data[6]}')
+
+for k in kid:
+    print(k)
